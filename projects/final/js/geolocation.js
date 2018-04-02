6de2c88c1d4a729401c5bba630d627bba6167ca4
Laurentiu Pteanc <lpetan2@uic.edu>
2018-04-02 19:01:31 +0000

    added geolocation watching

---
+++ projects/final/js/geolocation.js
@@ -0,0 +1,18 @@
+var id, options;
+
+function success(pos) {
+    console.log(pos);
+    updatePlayerLocation(pos);
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+options = {
+  enableHighAccuracy: true ,
+  timeout: 600000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
